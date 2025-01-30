315124393761549d15d8384a19dfc76282799451
shrutiy14 <syamala@ucsd.edu>
2025-01-29 22:36:13 -0800

    lab 4 changes 1

--- global.js
+++ global.js
@@ -89,3 +89,19 @@ if ("colorScheme" in localStorage) {
 } else {
   setColorScheme("light dark");
 }
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      
+      if (!response.ok) {
+          throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
