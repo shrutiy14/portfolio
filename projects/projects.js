8a709b69ce52f8c628130f14619076ecabf097d2
shrutiy14 <syamala@ucsd.edu>
2025-01-31 12:03:31 -0800

    project count

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,8 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `Projects Count: ${projects.length}`;
 
 /*import { fetchJSON, renderProjects } from '../global.js';
 
