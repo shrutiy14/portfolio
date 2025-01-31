d27dd2c685f601a43d247ea0a673cb1ac56b7f20
shrutiy14 <syamala@ucsd.edu>
2025-01-31 12:40:30 -0800

    debug

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
-<div class="projects"></div>
 renderProjects(latestProjects, projectsContainer, 'h2');
