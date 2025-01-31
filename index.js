67973133c09afd6d16a38b8159bb4d4240ca4605
shrutiy14 <syamala@ucsd.edu>
2025-01-31 15:36:03 -0800

    hey

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
+
 const githubData = await fetchGitHubData('shrutiy14');
 const profileStats = document.querySelector('#profile-stats');
 
