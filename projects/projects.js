02c83db71683a6e9ca44e46c41a60fd0b2977bd2
shrutiy14 <syamala@ucsd.edu>
2025-02-07 21:16:41 -0800

    legends

--- projects/projects.js
+++ projects/projects.js
@@ -12,22 +12,17 @@ let arc = arcGenerator({
         startAngle: 0,
         endAngle: 2 * Math.PI
 });
-let data = [1, 2, 3, 4, 5, 5];
-/*let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}*/
-// Use d3.pie() to generate the start and end angles for each slice
-let sliceGenerator = d3.pie();
+/*let data = [1, 2, 3, 4, 5, 5];*/
+let data = [
+    { value: 1, label: 'apples' },
+    { value: 2, label: 'oranges' },
+    { value: 3, label: 'mangos' },
+    { value: 4, label: 'pears' },
+    { value: 5, label: 'limes' },
+    { value: 5, label: 'cherries' },
+  ];
+let sliceGenerator = d3.pie().value((d) => d.value);
+/*let sliceGenerator = d3.pie();*/
 let arcData = sliceGenerator(data);  // This generates the start and end angles
 
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -38,6 +33,14 @@ arcs.forEach((arc, idx) => {
       .append('path')
       .attr('d', arc)
       .attr('fill', colors(idx));  // Fill each slice with a color
+})
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+    legend.append('li')
+          .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+          .attr('class', 'legend-item')
+          .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 })
 /*d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');*/
 
