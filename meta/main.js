0fc811594b426794ff43b03552d0a29e894cf0de
shrutiy14 <syamala@ucsd.edu>
2025-02-14 00:36:01 -0800

    change again

--- meta/main.js
+++ meta/main.js
@@ -1,10 +1,17 @@
 let data = [];
 
 async function loadData() {
-  data = await d3.csv('loc.csv');
-  console.log(data);
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
+
